3cf88081030a0f1ac8e43dff8208cc246a0e4f1a
takenori-y <takenori.yoshimura24@gmail.com>
2020-07-29 19:09:31 +0900

    second commit

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='horoscopy',
+    version='0.1.0',
+    description='Python module for speech signal processing',
+    author='Takenori Yoshimura',
+    author_email='takenori@sp.nitech.ac.jp',
+    url='https://github.com/takenori-y/horoscopy',
+    download_url='',
+    packages=find_packages(exclude=('tests')),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='speech signal dsp',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'librosa >= 0.8.0',
+        'numpy >= 1.15.0',
+        'scipy >= 1.5.0',
+    ],
+    extras_require={
+        'test': ['pytest'],
+    },
+)
